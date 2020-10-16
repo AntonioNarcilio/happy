bba683b62ecde319e0629a7e2d3e5fd5083bc332
Antonio Narclio <antonionarcilio@gmail.com>
2020-10-16 17:17:26 -0300

    🚀 Dia 4: Mobile - Maps dos orfanatos cadastrados

---
+++ mobile/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
+
+import Routes from './src/routes';
+
+export default function App() {
+  const [fontsLeaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  if (!fontsLeaded) {
+    return null;
+  }
+
+  return (
+    <Routes />
+  );
+}
+
