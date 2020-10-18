b2f4763f0fadfec7e96631c30a2c4a072588755d
Antonio Narclio <antonionarcilio@gmail.com>
2020-10-17 22:41:20 -0300

    Refatorando o código

--- backend/src/view/images_view.ts
+++ backend/src/view/images_view.ts
@@ -1,10 +1,17 @@
 import Image from '../models/Image';
 
+
+// Usando da aplicação web
+// const urlBase = 'localhost'
+// Usado na aplicação mobile
+const urlBase = '192.168.100.5'
+
 export default {
+
 	render (image: Image) {
 		return {
 			id: image.id,
-			url: `http://localhost:3333/uploads/${image.path}`
+			url: `http://${urlBase}:3333/uploads/${image.path}`
 		};
 	},
 
